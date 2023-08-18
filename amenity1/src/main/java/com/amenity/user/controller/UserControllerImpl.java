166b6e644edbd046f93a472376169e1dea55ac48
dlckdgus11 <chl970804@gmail.com>
2023-08-18 09:23:56 +0900

    ㅁㄴㅇ

--- amenity1/src/main/java/com/amenity/user/controller/UserControllerImpl.java
+++ amenity1/src/main/java/com/amenity/user/controller/UserControllerImpl.java
@@ -20,7 +20,7 @@ import com.amenity.user.vo.UserVO;
 
 /*2023.08.17 18:13 테스트입니다.*/
 /* mental 추가 테스트입니다.*/
-/* ㅇㅇ */
+/* ㅁㄴㅇㅇㅇ */
 @Controller("userController")
 public class UserControllerImpl {
 	@Autowired(required=true)
